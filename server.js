5ba21e040e32781ebd85c61279b8f0be881b6d9e
Łukasz Dawidowicz <lddev@outlook.com>
2021-02-02 14:35:14 +0100

    heroku build

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('api/db.json');
 const middlewares = jsonServer.defaults({
-  static: 'build',
+  static: 'dist',
   noCors: true,
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 server.use(middlewares);
 server.use(router);
